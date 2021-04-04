890165d587a1ab27636432ef3ad209917a8fc1ee
OktawiaRogowicz <oktawia.rogowicz.30@gmail.com>
2021-04-04 13:35:22 +0200

    Fixed lab2-4.py

--- Laboratorium 2/lab2-4.py
+++ Laboratorium 2/lab2-4.py
@@ -1,13 +1,28 @@
 import os
 
-
 # RED [1], GREEN [2], BLUE [3], YELLOW [4]
 import numpy as np
 
 
+def recognise_colour(prediction, expected_output):
+    index = np.argmax(prediction)
+    if index == 0:
+        print("RED")
+    elif index == 1:
+        print("GREEN")
+    elif index == 2:
+        print("BLUE")
+    else:
+        print("YELLOW")
+    index2 = np.argmax(expected_output)
+    if index == index2:
+        print("IT MATCHES!")
+
+
 class NeuralNetwork:
-    def __init__(self):
+    def __init__(self, weights):
         self.alpha = 0.01
+        self.weights = weights[0]
 
     def read(self, filename, n):
         THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -16,18 +31,23 @@ class NeuralNetwork:
         self.count_series(n, file)
         file.close()
 
-    def one_series(self, n, input, weights, expected_output):
+    def one_series(self, input, expected_output):
         A = input
-        B = weights
+        # print(A)
+        B = self.weights
+        # print(B)
         C = A.dot(B.transpose())
         prediction = C
         # print("C: \n", C, "\n\n")
+        # print(expected_output)
+
+        recognise_colour(prediction, expected_output)
 
         error = pow(prediction - expected_output, 2)
         delta = prediction - expected_output
         # print("delta: \n", delta)
-        weight_delta = np.outer(delta, input[n])
-        weights = weights - weight_delta * self.alpha
+        weight_delta = np.outer(delta, input)
+        self.weights = self.weights - weight_delta * self.alpha
         # print("wd: \n", weight_delta, "\nweights: \n", self.weights)
         # print("error: \n", error, "\n")
         return error
@@ -42,35 +62,32 @@ class NeuralNetwork:
             return np.array([0.0, 0.0, 1.0, 0.0])
         return np.array([0.0, 0.0, 0.0, 1.0])
 
-    def calc_weights(self, input, expected_output):
-        print("Input: \n", input)
-        print("Exp out: \n", expected_output)
-        weights = np.array([input * expected_output[0],
-                           input * expected_output[1],
-                           input * expected_output[2],
-                           input * expected_output[3]])
-        print("WEIGHTS: \n", weights)
-        return weights
-
     def count_series(self, n, file):
         error = 0.0
         for j in range(n):
+            file.seek(0)
             for line in file:
                 fields = line.strip().split()
 
                 input = np.array([float(fields[0]), float(fields[1]), float(fields[2])])
                 expected_output = self.calc_exp_output(float(fields[3]))
-                weights = self.calc_weights(input, expected_output)
-
-                # error = error + self.one_series(input, weights, expected_output)
+                error = error + self.one_series(input, expected_output)
             # print(error, "\n")
-            # print(error[0] + error[1] + error[2])
             error = 0.0
 
 
 if __name__ == '__main__':
     test_colors = 'test_colors.txt'
     training_colors = 'training_colors.txt'
-    nn = NeuralNetwork()
-    nn.read(training_colors, 1)
-
+    weights = np.array([[[0.26984179, 0.71716623, 0.67143705],
+                        [0.7975498, 0.1757912, 0.33016793],
+                        [0.7547338, 0.84341124, 0.35006583],
+                        [0.02574414, 0.14325726, 0.40649936]],
+                       [[0.26984179, 0.71716623, 0.67143705],
+                       [0.7975498, 0.1757912, 0.33016793],
+                       [0.7547338, 0.84341124, 0.35006583],
+                       [0.02574414, 0.14325726, 0.40649936]]])
+    nn = NeuralNetwork(weights)
+    nn.read(training_colors, 50)
+    print("-----------------------TEST-------------------------")
+    nn.read(test_colors, 1)
